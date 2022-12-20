1a5b0bcc5ef16506ae698be8611350b30d31d66f
Andrey Engo <5522662@gmail.com>
2022-12-21 00:18:21 +0300

    add HomeWork001

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите трехзнчное число - ");
+int number = Convert.ToInt32(ReadLine());
+int digit = number / 10;
+int digit2 = digit % 10;
+    
+WriteLine(Math.Abs(number) > 999 || Math.Abs(number) < 100?     // Проверка является ли число трехзначным
+     "Вы ввели не трехзначное число": $"{Math.Abs(digit2)}");   // Добавил метод Math.Abs, чтобы число не вводилось со знаком минус, если такое введут
+
+
+
+

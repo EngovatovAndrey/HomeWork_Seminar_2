3535a5566ac6bafa84f63b8b4c4d654395980543
Andrey Engo <5522662@gmail.com>
2022-12-21 13:18:17 +0300

    optim code

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -6,11 +6,11 @@ Clear();
 
 WriteLine("Введите трехзначное число - ");
 int number = Convert.ToInt32(ReadLine());
-int digit = number / 10;
-int digit2 = digit % 10;
+int digit = (number / 10)% 10;
+
     
 WriteLine(Math.Abs(number) > 999 || Math.Abs(number) < 100?     // Проверка является ли число трехзначным
-     "Вы ввели не трехзначное число": $"{Math.Abs(digit2)}");   // Добавил метод Math.Abs, чтобы число не вводилось со знаком минус, если такое введут
+     "Вы ввели не трехзначное число": $"{Math.Abs(digit)}");   // Добавил метод Math.Abs, чтобы число не вводилось со знаком минус, если такое введут
 
 
 
